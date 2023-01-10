8c872a6da101dea94438bcc6f7fcaf68dee67278
JEONG GIHOON <rlgns927@gmail.com>
2023-01-10 16:41:56 +0900

    Test

---
+++ app/src/main/java/git/myapplication/jgh_prgorammergittest/MainActivity.kt
@@ -0,0 +1,11 @@
+package git.myapplication.jgh_prgorammergittest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
